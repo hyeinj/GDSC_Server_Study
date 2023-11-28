6b47af4525c1a2b07bac54f671b029f280519ef4
Ubuntu <jhi2002@hanyang.ac.kr>
2023-11-28 10:03:13 +0000

    postLogin 함수 구현

--- server_nodejs/src/app/User/userService.js
+++ server_nodejs/src/app/User/userService.js
@@ -3,6 +3,7 @@ const {pool} = require("../../../config/database");
 const secret_config = require("../../../config/secret");
 const userProvider = require("./userProvider");
 const userDao = require("./userDao");
+const jwtMake = require("../../../config/jwtUtils");
 const baseResponse = require("../../../config/baseResponseStatus");
 const {response} = require("../../../config/response");
 const {errResponse} = require("../../../config/response");
@@ -39,6 +40,61 @@ exports.createUser = async function(username, userID, password, email){
         return response(baseResponse.SUCCESS);
     } catch(err){
         logger.error(`App - createUser Service error\n: ${err.message}`);
+        return errResponse(baseResponse.DB_ERROR);
+    }
+};
+
+//로그인 인증 방법(JWT)
+exports.postLogin = async function (email, password){
+    try{
+        //이메일 여부 확인
+        const emailRows = await userProvider.emailCheck(email);
+        if (emailRows.length < 1) return errResponse(baseResponse.SIGNIN_EMAIL_WRONG);
+
+        const selectEmail = emailRows[0].u_email
+
+        // 비밀번호 확인
+        const hashedPassword = await crypto
+        .createHash("sha512")
+        .update(password)
+        .digest("hex")
+
+        const selectUserPasswordParams = [selectEmail, hashedPassword];
+        const passwordRows = await userProvider.passwrodCheck(selectUserPasswordParams);
+
+        if(passwordRows[0].u_pw !== hashedPassword){
+            return errResponse(baseResponse.SIGNIN_PASSWORD_WRONG);
+        }
+
+        //계정 상태 확인
+        const userInfoRows = await userProvider.accountCheck(email);
+
+        if(userInfoRows[0].status == 0){
+            return errResponse(baseResponse.SIGNIN_INACTIVE_ACCOUNT);
+        } else if(userInfoRows[0].status == 2){
+            return errResponse(baseResponse.SIGNIN_WITHDRAWAL_ACCOUNT);
+        }
+
+        let uid = userInfoRows[0].uid;
+        
+        let token = jwtMake.makeAccessToken(uid);
+        console.log("access token :" + token);
+
+        let refreshToken = jwtMake.makeRefreshToken();
+        console.log("refresh token : " + refreshToken);
+
+        const updateRefreshTokenParams = [refreshToken, uid];
+
+        const connection = await pool.getConnection(async (conn) => conn);
+
+        const refreshTokenResult = await userDao.updateRefreshToken(connection, updateRefreshTokenParams);
+        console.log(`refresh token 입력 : ${uid}`);
+        connection.release();
+
+        return response(baseResponse.SUCCESS, {'userId': userInfoRows[0].uid, 'jwt-accessToken': token, 'jwt-refreshToken': refreshToken});
+
+    } catch(err){
+        logger.error(`App - postLogin Service error\n: ${err.message} \n${JSON.stringify(err)}`);
         return errResponse(baseResponse.DB_ERROR);
     }
 };
